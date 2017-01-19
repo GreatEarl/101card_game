f31cff795a14287ecaf92774f385db60a98b76a8
RY <great.earl@gmail.com>
2017-01-19 17:15:08 +0600

    results table

--- main.js
+++ main.js
@@ -41,70 +41,115 @@ var closedAr = [];
 var playerAr = [];
 var iiAr = [];
 var usedindex = [];
+var gameOver = false;
+
+var resArr = [];
+
+var iiResult;
+var playerResult;
+
 
 var i=0;
 $("#backs").on("click", function() {
  
 // alert($("#cards").html());
-  
-if (0<closedAr.length) {
-    openedAr.push(closedAr.pop());
-    setOpened();
+if (!gameOver) {
+    if (0<closedAr.length ){
+        playerAr.push(closedAr.pop());
+        setHand();
+      } 
+      else 
+      { 
+        refillClosed ();
+        setOpened();
+      }
+    }
+else {
+  return;
+ }
+
+});
+
+function refillClosed (){
+    var lastCard = openedAr.pop();
+  //   console.log(lastCard);
+    for (var k=0; k<openedAr.length; k++) {
+       closedAr.push(openedAr.pop());
+     }
+    openedAr.push(lastCard);
+};
+
+function setiiArr(){
+ if (!gameOver) { 
+  if (0<closedAr.length) {
+    iiAr.push(closedAr.pop());
+    setII();
    } 
    else 
    { 
-     $("#cards").html('end of stock - click refill');
+        refillClosed ();
+        setOpened();
    }
-  
-  
-  
-  
-})
+       }
+else {
+  return;
+ }
+};
+
 function randomInteger(min, max) {
     var rand = min + Math.random() * (max + 1 - min);
     rand = Math.floor(rand);
     return rand;
   };
 
-function initgame() {
+
+function initround() {
  var ri = 0;
  var cli=0;
- closedAr.length = 0;
- usedindex.length = 0;
 
+ closedAr.length = 0;
+ openedAr.length = 0;
+ usedindex.length = 0;
  playerAr.length = 0;
  iiAr.length = 0;
+ 
+ gameOver = false;
+
+ console.log('initround');
+
+
  do { 
   ri = randomInteger(0, 35) ;
   if (usedindex.indexOf(ri) == '-1'){
-    closedAr.push(stock[ri]);
-   cli++;
+      closedAr.push(stock[ri]);
+      cli++;
       usedindex.push(ri);
-      }
-      else {
+   }
+  else {
          continue;
-      }
-   
-  }
-  while (closedAr.length < 36);
+       }
+ }
+ 
+ while (closedAr.length < 36);
  // alert( closed.length );
-   console.log (closedAr.length);
+ console.log (closedAr.length);
 
 var ti = 0;
 ti=randomInteger(0,1);
 
 if (ti == 0) {
   $("#move").html('me');
+   console.log('ii set');
+   for (var i=0; i<4;i++) {
+      iiAr.push(closedAr.pop());
+   }
 
-for (var i=0; i<4;i++) {
-  iiAr.push(closedAr.pop());
-}
-for (var i=0; i<5;i++) {
-  playerAr.push(closedAr.pop());
-}
-
+   for (var i=0; i<5;i++) {
+      playerAr.push(closedAr.pop());
+   }
 }
 else {
+  console.log('player set');
   $("#move").html('you');
   for (var i=0; i<5;i++) {
      iiAr.push(closedAr.pop());
@@ -115,16 +160,39 @@ else {
   }
 openedAr.push(closedAr.pop());
 
-
-
-setHand();
-
-setOpened();
+console.log('initround2');
+ setHand();
+ setII();
+ setOpened();
+ $("#cards").html('stock');
 
 }
 
-function setOpened () {
 
+function initgame() {
+
+
+
+ iiResult = 0;
+ playerResult = 0;
+ 
+ resArr.length = 0;
+
+
+    
+
+$("#resBody").html('<tr><td></td><td></td> <td></td> </tr>');
+
+
+
+
+//first round
+ initround();
+$("#next").prop('disabled', !gameOver);
+}
+
+function setOpened () {
+if (!gameOver) {
 
 opencont = '<div id ="cardc" class = "card" >'+openedAr[openedAr.length-1].name+ '</div>'+
            '<div id="num"> '+openedAr[openedAr.length-1].suitcode +' </div>';
@@ -134,7 +202,7 @@ opencont = '<div id ="cardc" class = "card" >'+openedAr[openedAr.length-1].name+
  
  $("#num").css("color",openedAr[openedAr.length-1].color);
  $("#cardc").css("color",openedAr[openedAr.length-1].color);
-
+}
 
 }
 
@@ -143,16 +211,47 @@ opencont = '<div id ="cardc" class = "card" >'+openedAr[openedAr.length-1].name+
 
 
 function setHand() {
-var handcont ='';
-for (var j=0;j< playerAr.length; j++) {
-handcont = handcont + 
-' <div id="'+playerAr[j].name +'of'+playerAr[j].suit  +'" class ="cardh" >'+playerAr[j].suitcode +
-   '<div id="sname" > '+playerAr[j].name +'</div>'+
+
+   
+    var handcont ='';
+    for (var j=0;j< playerAr.length; j++) {
+    handcont = handcont + 
+    ' <div id="'+playerAr[j].name +'of'+playerAr[j].suit  +'" class ="cardh" >'+playerAr[j].suitcode +
+    '<div id="sname" > '+playerAr[j].name +'</div>'+
+    '</div>'
+    }
+    $("#hand").html(handcont);
+  
+  if (playerAr.length == 0) {
+     console.log('game over');
+     gameOver = true;
+      displayResult ('player');
+  }
+}
+
+
+
+
+
+function setII() {
+var IIcont ='';
+for (var j=0;j< iiAr.length; j++) {
+IIcont = IIcont + 
+' <div id="'+iiAr[j].name +'of'+iiAr[j].suit  +'" class ="cardii" > &#x2169'+
+   '<div id="sname" > &#x2169 </div>'+
    '</div>'
+}
+
+$("#ii").html(IIcont);
+
+if (iiAr.length == 0) {
+     console.log('game over');
+    gameOver = true;
+    displayResult ('ii');
+  }
 
 }
-$("#hand").html(handcont);
-}
+
 
 function releasecard () {
    var target = $(this).attr('id')
@@ -181,18 +280,116 @@ setHand();
 
 
 function checkArr() {
-   console.log(closedAr.length);
- // for (var i=0; i< closedAr.length; i++) {
-//    console.log(closedAr[i]);
-//  }
-for (var i=0; i< playerAr.length; i++) {
-    console.log(playerAr[i]);
-  }
-    console.log('-----');
-  for (var i=0; i< iiAr.length; i++) {
-    console.log(iiAr[i]);
-  }
 
+
+if (!gameOver) {
+openedAr.push(iiAr.pop());
+};
+
+setOpened();
+setII();
 }
 
+function displayResult (lastMove) {
 
+console.log(lastMove);
+
+  if (lastMove === 'player') {
+    playerResult = playerResult + 0;
+    //iiResult
+    for (var i=0; i< iiAr.length; i++) {
+      switch (iiAr[i].name) {
+        case "6":
+          iiResult = iiResult+6;
+          break;
+        case "7":
+          iiResult = iiResult+7;
+          break;
+        case "8":
+          iiResult = iiResult+8;
+          break;
+        case "9":
+          break;
+        case "10":
+          iiResult = iiResult+10;
+          break;
+        case "J":
+          iiResult = iiResult+2;
+          break;
+        case "Q":
+          iiResult = iiResult+3;
+          break;
+        case "K":
+          iiResult = iiResult+4;
+          break;
+        case "A":
+          iiResult = iiResult+11;
+          break;
+      }
+    }
+
+
+   }
+   else {
+      iiResult = iiResult + 0;
+
+      for (var i=0; i< playerAr.length; i++) {
+      switch (playerAr[i].name) {
+        case "6":
+          playerResult = playerResult+6;
+          break;
+        case "7":
+          playerResult = playerResult+7;
+          break;
+        case "8":
+          playerResult = playerResult+8;
+          break;
+        case "9":
+          break;
+        case "10":
+          playerResult = playerResult+10;
+          break;
+        case "J":
+          playerResult = playerResult+2;
+          break;
+        case "Q":
+          playerResult = playerResult+3;
+          break;
+        case "K":
+          playerResult = playerResult+4;
+          break;
+        case "A":
+          playerResult = playerResult+11;
+          break;
+      }
+    }
+   }
+
+console.log(iiResult);
+console.log(playerResult);
+
+$("#next").prop('disabled', !gameOver);
+var resElem = {};
+resElem.ii = iiResult;
+resElem.player = playerResult;
+resArr.push(resElem);
+
+var bodyCont = '';
+for (var i = 0; i < resArr.length; i++) {
+var j=i+1;
+console.log(resArr[i]);
+
+  bodyCont = bodyCont+
+   
+        ' <tr>  <td>'+ j +'</td>'+
+        '<td>'+ resArr[i].player+'</td>'+
+        '<td>'+ resArr[i].ii+'</td> </tr>';
+
+ }
+   //bodyCont = bodyCont+'';
+
+$("#resBody").html(bodyCont);
+
+
+
+}
